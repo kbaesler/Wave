a62cdbc674e762d77691f11f5694eb94b6739887
Jumpercables <kyle.baesler@gmail.com>
2014-10-20 10:31:08 -0700

    Moved the ReentrancyMonitor to it's own class file.

--- src/Wave.Extensions.Esri/ESRI/ArcGIS/Geodatabase/Extensions/RowExtensions.cs
+++ src/Wave.Extensions.Esri/ESRI/ArcGIS/Geodatabase/Extensions/RowExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace ESRI.ArcGIS.Geodatabase
@@ -304,65 +305,6 @@ namespace ESRI.ArcGIS.Geodatabase
                 if (reentrancyMonitor.IsBusy)
                     throw new InvalidOperationException("There was a call to BlockReentrancy of which the IDisposable return value has not yet been disposed of.");
             }
-        }
-
-        #endregion
-
-        #region Nested Type: ReentrancyMonitor
-
-        /// <summary>
-        ///     An internal class used to interupt a method in the middle of its execution and then safely called again
-        ///     ("re-entered") before its previous invocations complete execution.
-        ///     The interruption could be caused by an internal action such as a jump or call, or by an external action such as a
-        ///     hardware interrupt or signal. Once the reentered invocation completes, the previous invocations will resume correct
-        ///     execution.
-        /// </summary>
-        private class ReentrancyMonitor : IDisposable
-        {
-            #region Fields
-
-            private int _BusyCount;
-
-            #endregion
-
-            #region Public Properties
-
-            /// <summary>
-            ///     Gets a value indicating whether this instance is busy.
-            /// </summary>
-            /// <value>
-            ///     <c>true</c> if this instance is busy; otherwise, <c>false</c>.
-            /// </value>
-            public bool IsBusy
-            {
-                get { return _BusyCount > 0; }
-            }
-
-            #endregion
-
-            #region IDisposable Members
-
-            /// <summary>
-            ///     Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
-            /// </summary>
-            public void Dispose()
-            {
-                --_BusyCount;
-            }
-
-            #endregion
-
-            #region Public Methods
-
-            /// <summary>
-            ///     Increments the busy counter for the monitor.
-            /// </summary>
-            public void Set()
-            {
-                ++_BusyCount;
-            }
-
-            #endregion
         }
 
         #endregion
---
+++ src/Wave.Extensions/System/Collections/ReentrancyMonitor.cs
@@ -0,0 +1,57 @@
+﻿namespace System.Collections
+{
+    /// <summary>
+    ///     A sealed class used to interupt a method in the middle of its execution and then safely called again
+    ///     ("re-entered") before its previous invocations complete execution.
+    ///     The interruption could be caused by an internal action such as a jump or call, or by an external action such as a
+    ///     hardware interrupt or signal. Once the reentered invocation completes, the previous invocations will resume correct
+    ///     execution.
+    /// </summary>
+    public sealed class ReentrancyMonitor : IDisposable
+    {
+        #region Fields
+
+        private int _BusyCount;
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        ///     Gets a value indicating whether this instance is busy.
+        /// </summary>
+        /// <value>
+        ///     <c>true</c> if this instance is busy; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsBusy
+        {
+            get { return _BusyCount > 0; }
+        }
+
+        #endregion
+
+        #region IDisposable Members
+
+        /// <summary>
+        ///     Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            --_BusyCount;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        ///     Increments the busy counter for the monitor.
+        /// </summary>
+        public void Set()
+        {
+            ++_BusyCount;
+        }
+
+        #endregion
+    }
+}
