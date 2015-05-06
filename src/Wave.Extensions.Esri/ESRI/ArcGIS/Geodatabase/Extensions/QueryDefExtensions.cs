edcdea48a2b35fd047ce1711683295751a83e649
Kyle <kyle.baesler@gmail.com>
2015-05-06 15:20:16 -0700

    Added Count method for IQueryDef

---
+++ src/Wave.Extensions.Esri/ESRI/ArcGIS/Geodatabase/Extensions/QueryDefExtensions.cs
@@ -0,0 +1,46 @@
+﻿using ESRI.ArcGIS.ADF;
+
+namespace ESRI.ArcGIS.Geodatabase.Extensions
+{
+    /// <summary>
+    ///     Provides extension methods for the <see cref="IQueryDef" /> interface.
+    /// </summary>
+    public static class QueryDefExtensions
+    {
+        #region Public Methods
+
+        /// <summary>
+        ///     Returns the number of records affected by executing the query definition.
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <returns>
+        ///     Returns a <see cref="int" /> representing the number of records affected.
+        /// </returns>
+        public static int Count(this IQueryDef source)
+        {
+            string subFields = source.SubFields;
+
+            try
+            {
+                using (ComReleaser cr = new ComReleaser())
+                {
+                    source.SubFields = "COUNT(*)";
+
+                    ICursor cursor = source.Evaluate();
+                    cr.ManageLifetime(cursor);
+
+                    IRow row = cursor.NextRow();
+                    if (row != null) return row.GetValue(0, 0);
+                }
+
+                return 0;
+            }
+            finally
+            {
+                source.SubFields = subFields;
+            }
+        }
+
+        #endregion
+    }
+}
